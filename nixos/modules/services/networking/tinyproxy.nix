aa262d695ebe4b585a2901b8a0f1cf8831a241b4
Andrew Marshall <andrew@johnandrewmarshall.com>
2023-11-16 11:16:11 -0500

    nixos/tinyproxy: Fix incorrect doc example
    It should be a Nix expression, but was not.

--- nixos/modules/services/networking/tinyproxy.nix
+++ nixos/modules/services/networking/tinyproxy.nix
@@ -34,12 +34,12 @@ in
         description = "Configuration for [tinyproxy](https://tinyproxy.github.io/).";
         default = { };
         example = literalExpression ''{
-          Port 8888;
-          Listen 127.0.0.1;
-          Timeout 600;
-          Allow 127.0.0.1;
-          Anonymous = ['"Host"' '"Authorization"'];
-          ReversePath = '"/example/" "http://www.example.com/"';
+          Port = 8888;
+          Listen = "127.0.0.1";
+          Timeout = 600;
+          Allow = "127.0.0.1";
+          Anonymous = ["\"Host\"" "\"Authorization\""];
+          ReversePath = "\"/example/\"" "\"http://www.example.com/\"";
         }'';
         type = types.submodule ({name, ...}: {
           freeformType = settingsFormat.type;
