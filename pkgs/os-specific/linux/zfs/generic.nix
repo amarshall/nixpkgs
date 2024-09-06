amarshall/nixpkgs

commit 636134b8ed2112412c1da441b4cc4d47a24e3bd3

zfs_2_2: 2.2.5 -> 2.2.6
- This release brings official support for Kernel 6.10
- Also update zfs_unstable as it would otherwise lag behind
- Upstream now links `zpool` completion, so only do so on older zfs
  (since we still need to support 2.1.x)

Changelog: https://github.com/openzfs/zfs/releases/tag/zfs-2.2.6

pkgs/os-specific/linux/zfs/generic.nix
@@ -184,6 +184,7 @@ let
 
       # Add Bash completions.
       install -v -m444 -D -t $out/share/bash-completion/completions contrib/bash_completion.d/zfs
+    '' + optionalString (lib.versionOlder version "2.2.6") ''
       (cd $out/share/bash-completion/completions; ln -s zfs zpool)
     '';
 

pkgs/os-specific/linux/zfs/2_2.nix
@@ -14,10 +14,10 @@ callPackage ./generic.nix args {
   # this attribute is the correct one for this package.
   kernelModuleAttribute = "zfs_2_2";
   # check the release notes for compatible kernels
-  kernelCompatible = kernel: kernel.kernelOlder "6.10";
+  kernelCompatible = kernel: kernel.kernelOlder "6.11";
 
   # this package should point to the latest release.
-  version = "2.2.5";
+  version = "2.2.6";
 
   tests = [
     nixosTests.zfs.installer
@@ -26,5 +26,5 @@ callPackage ./generic.nix args {
 
   maintainers = with lib.maintainers; [ adamcstephens amarshall ];
 
-  hash = "sha256-BkwcNPk+jX8CXp5xEVrg4THof7o/5j8RY2SY6+IPNTg=";
+  hash = "sha256-wkgoYg6uQOHVq8a9sJXzO/QXJ6q28l7JXWkC+BFvOb0=";
 }

pkgs/os-specific/linux/zfs/unstable.nix
@@ -20,12 +20,12 @@ callPackage ./generic.nix args {
   # IMPORTANT: Always use a tagged release candidate or commits from the
   # zfs-<version>-staging branch, because this is tested by the OpenZFS
   # maintainers.
-  version = "2.2.5";
+  version = "2.2.6";
   # rev = "";
 
   tests = [
     nixosTests.zfs.unstable
   ];
 
-  hash = "sha256-BkwcNPk+jX8CXp5xEVrg4THof7o/5j8RY2SY6+IPNTg=";
+  hash = "sha256-wkgoYg6uQOHVq8a9sJXzO/QXJ6q28l7JXWkC+BFvOb0=";
 }

commit 89dc1831583d225291dc65bbfc70fd8b133c46b7

zfs_2_1: fix building kernel module
Building the user package still worked.

Broken in 636134b8ed2112412c1da441b4cc4d47a24e3bd3.

pkgs/os-specific/linux/zfs/generic.nix
@@ -184,7 +184,7 @@ let
 
       # Add Bash completions.
       install -v -m444 -D -t $out/share/bash-completion/completions contrib/bash_completion.d/zfs
-    '' + optionalString (lib.versionOlder version "2.2.6") ''
+    '' + optionalString (buildUser && (lib.versionOlder version "2.2.6")) ''
       (cd $out/share/bash-completion/completions; ln -s zfs zpool)
     '';
 